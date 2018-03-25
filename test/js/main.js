8674aa478dce1f974e66b166ca00924d4a2046cc
Gitto K Tomy <gittokpr@gmail.com>
2018-03-25 13:35:33 +0530

    added some js check on 1st signup

--- test/js/main.js
+++ test/js/main.js
@@ -12,3 +12,15 @@
 
     }); // end of document ready
 })(jQuery);
+
+function count(){
+    var val = document.getElementById('password').value;
+    if(val.length<7){
+        document.getElementById('pd_1').innerHTML = "Password (min 7 character)";
+        document.getElementById('pd_1').style.color = "red";
+    }
+    else{
+        document.getElementById('pd_1').innerHTML = "Password";
+        document.getElementById('pd_1').style.color = "#26a69a";
+    }
+}
